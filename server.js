5a06b3245d2c388f9b5bde81e12d164439c3c252
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-09-23 17:23:11 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors({ optionsSuccessStatus: 200 }))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/whoami', (req, res) => {
+  const ipaddress = req.ip
+  const language = req.headers['accept-language']
+  const software = req.headers['user-agent']
+  res.json({ ipaddress, language, software })
+})
+
+app.listen(process.env.PORT ?? 3000, () => {
+  console.info('Server is up')
+})
